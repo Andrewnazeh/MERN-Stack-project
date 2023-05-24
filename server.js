40ff9918e62b11c2b623a11238120e80c4234ea5
basm1610 <86631963+basm1610@users.noreply.github.com>
2023-05-25 01:57:39 +0200

    create department branch with backend and frontend

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ import express from "express";
 import mongoose from "mongoose";
 import { engine } from "express-handlebars";
 import dotenv from "dotenv";
+import routerDepartments from './routers/departments.js';
 dotenv.config();
 
 mongoose.connect(process.env.mongConnection);
 
 const app = express();
+// ++++++++ Using this function to link the css file with file temlates ++++++++ //
+app.use(express.static('public'));
+
+// ++++++++ using to be req.body in controller ++++++++ //
+app.use(express.urlencoded({extended:true}));
+
+// ++++++++ Using route of departments ++++++++ //
+app.use("/departments", routerDepartments);
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 
-
+// ++++++++ When run project main screen this http://localhost:3000 ++++++++ // 
+app.use("/",(req,res)=>{
+    res.render('adminstrator/admin');
+})
 
 
 app.listen(process.env.port, () => {
